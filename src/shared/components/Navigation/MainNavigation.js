a33d8b016f9d1e5352d60e0fa08953cb28294226
Suman Kumari <sumankri5501@gmail.com>
2020-12-10 19:00:48 +0530

    nav-links added

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import MainHeader from './MainHeader';
 import './MainNavigation.css';
+import NavLink from './NavLinks';
 
 const MainNavigation = props => {
     return <MainHeader>
@@ -15,7 +16,7 @@ const MainNavigation = props => {
             <Link to="/">Your Places</Link>
         </h1>
         <nav>
-            ....
+        <NavLink />
         </nav>
     </MainHeader>
 }
